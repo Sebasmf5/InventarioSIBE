686d63aeae772546bc3dd59f1aa0bac448d89d21
Sebastian <sebastian.morales01@uceva.edu.co>
2026-08-01 17:30:28 -0500

    Initial commit

---
+++ src/main/java/co/edu/uceva/inventariosibe/InventarioSibeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.inventariosibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioSibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioSibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uceva/inventariosibe/InventarioSibeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.inventariosibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
